defb0441f3d435902d72065772a3a79f65107c61
Oscar Bridge <30382493+oscarrbridge@users.noreply.github.com>
2025-08-24 19:01:31 +1200

    Created a basic navbar
    Added Login / Register buttons
    
    Added Login / Register pop up boxes

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 
-import Home from './pages/Home/home.tsx'
+import Home from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
