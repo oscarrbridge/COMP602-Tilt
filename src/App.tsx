105dcaa7e1c829dee9604a7ac80e920ae24d2f2b
Oscar Bridge <oscarbridge.nz@gmail.com>
2025-08-26 16:40:51 +1200

    Added filter bar to home screen
    Added game categories to front page

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,68 @@
-import NavBar from './components/NavBar/NavBar.tsx'
-import SpecialEvent from './components/SpecialEvent/SpecialEvent.tsx'
-import GameCard from './components/GameCard/GameCard.tsx'
-import SearchBar from './components/SearchBar/SearchBar.tsx'
+import NavBar from "./components/NavBar/NavBar.tsx";
+import SpecialEvent from "./components/SpecialEvent/SpecialEvent.tsx";
+import GameCard from "./components/GameCard/GameCard.tsx";
+import SearchBar from "./components/SearchBar/SearchBar.tsx";
+import FilterBar from "./components/FilterBar/FilterBar.tsx"
 
-import './App.css'
+import "./App.css";
 
 // Required npm packages: react-router-dom
 
 const SpecialEvents = [
   {
-    EventHook: 'Hook',
-    EventTitle: 'Title',
-    EventDescription: 'Description',
-    EventImage: 'src/assets/Tilt.png',
-    EventLink: '/'
+    EventHook: "Hook",
+    EventTitle: "Title",
+    EventDescription: "Description",
+    EventImage: "src/assets/Tilt.png",
+    EventLink: "/",
   },
   {
-    EventHook: 'Hook',
-    EventTitle: 'Title',
-    EventDescription: 'Description',
-    EventImage: 'src/assets/Tilt.png',
-    EventLink: '/'
+    EventHook: "Hook",
+    EventTitle: "Title",
+    EventDescription: "Description",
+    EventImage: "src/assets/Tilt.png",
+    EventLink: "/",
   },
   {
-    EventHook: 'Hook',
-    EventTitle: 'Title',
-    EventDescription: 'Description',
-    EventImage: 'src/assets/Tilt.png',
-    EventLink: '/'
-  }
-]
+    EventHook: "Hook",
+    EventTitle: "Title",
+    EventDescription: "Description",
+    EventImage: "src/assets/Tilt.png",
+    EventLink: "/",
+  },
+];
 
 const PopularGames = [
   {
-    Text: 'Slots',
-    Image: 'src/assets/Tilt.png'
+    Text: "Slots",
+    Image: "src/assets/Tilt.png",
   },
   {
-    Text: 'Slots',
-    Image: 'src/assets/Tilt.png'
+    Text: "Slots",
+    Image: "src/assets/Tilt.png",
   },
   {
-    Text: 'Slots',
-    Image: 'src/assets/Tilt.png'
+    Text: "Slots",
+    Image: "src/assets/Tilt.png",
   },
   {
-    Text: 'Slots',
-    Image: 'src/assets/Tilt.png'
+    Text: "Slots",
+    Image: "src/assets/Tilt.png",
   },
-
-]
+];
 
 export default function App() {
-
   return (
     <>
-      <div className='NavBar'>
+      <div className="NavBar">
         <NavBar />
       </div>
 
-      <div className='SpecialEventsContainer'>
-        <div className='SpecialEventsTitle'>
+      <div className="SpecialEventsContainer">
+        <div className="SpecialEventsTitle">
           <h2>Special Events</h2>
         </div>
-        <div className='SpecialEvents'>
+        <div className="SpecialEvents">
           {SpecialEvents.map((event, index) => (
             <SpecialEvent key={index} {...event} />
           ))}
@@ -71,29 +70,59 @@ export default function App() {
 
         <br />
 
-        <div className='PopularGamesContainer'>
-          <div className='PopularGamesTitle'>
+        <div className="GamesContainer">
+          <div className="GamesTitle">
             <h2>Popular Games</h2>
           </div>
-          <div className='PopularGames'>
+          <div className="Games">
             {PopularGames.map((game, index) => (
               <GameCard key={index} {...game} />
             ))}
           </div>
         </div>
 
-            <br />
+        <br />
 
-        <div className='HomeSearchBarContainer'>
-          <div className='HomeSearchBarTitle'>
+        <div className="HomeSearchBarContainer">
+          <div className="HomeSearchBarTitle">
             <h2>Looking for a game?</h2>
           </div>
-          <div className='HomeSearchBar'>
-            <SearchBar Placeholder='Search for a game...'/>
+          <div className="HomeSearchBar">
+            <SearchBar Placeholder="Search for a game..." />
           </div>
         </div>
 
+        <br />
+
+            <div className="FilterBar">
+              <FilterBar />
+            </div>
+
+        <br />
+
+        <div className="GamesContainer">
+          <div className="GamesTitle">
+            <h2>Sports</h2>
+          </div>
+          <div className="Games">
+            {PopularGames.map((game, index) => (
+              <GameCard key={index} {...game} />
+            ))}
+          </div>
+        </div>
+
+
+        <div className="GamesContainer">
+          <div className="GamesTitle">
+            <h2>Slots</h2>
+          </div>
+          <div className="Games">
+            {PopularGames.map((game, index) => (
+              <GameCard key={index} {...game} />
+            ))}
+          </div>
+        </div>
       </div>
     </>
-  )
+  );
 }
