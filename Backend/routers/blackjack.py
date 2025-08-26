9bdea2fcb80d99a201ad1af17cf15d01d8a9ff24
Sam <sw.samuel.10@gmail.com>
2025-08-27 10:33:13 +1200

    gitignore and requirements.txt

--- Backend/routers/blackjack.py
+++ Backend/routers/blackjack.py
@@ -3,13 +3,16 @@ from Backend import blackjack
 
 router = APIRouter(prefix="/blackjack", tags=["blackjack"])
 
+
 @router.post("/start")
 def start_game():
     return blackjack.start_game()
 
+
 @router.post("/hit")
 def hit(state: blackjack.GameState):
     return blackjack.hit(state)
+
 
 @router.post("/stand")
 def stand(state: blackjack.GameState):
