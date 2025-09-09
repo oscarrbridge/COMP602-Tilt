13762eba5d52e8a1f8a9d6b94b4b4eb20de9eb44
Oscar Bridge <oscarbridge.nz@gmail.com>
2025-09-09 19:11:57 +1200

    Changed 'Deposit' page to 'Wallet'

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Home from "./App.tsx";
-import Deposit from "./pages/Deposit/Deposit.tsx";
+import Wallet from "./pages/Wallet/Wallet.tsx";
 import Statistics from "./pages/Statistics/Statistics.tsx";
 import Blackjack from "./Games/Blackjack/Blackjack.tsx";
 import Slots from "./Games/Slots/slots.tsx";
@@ -15,7 +15,7 @@ createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/deposit" element={<Deposit />} />
+        <Route path="/wallet" element={<Wallet />} />
         <Route path="/leaderboard" element={<Leaderboard />} />
         <Route path="/statistics" element={<Statistics />} />
         <Route path="/slots" element={<Slots />} />
---
+++ src/pages/Wallet/Wallet.tsx
@@ -0,0 +1,18 @@
+import './Wallet.css'
+import NavBar from '../../components/NavBar/NavBar.tsx'
+import Withdraw from '../../components/Withdraw/Withdraw.tsx'
+
+export default function Wallet()
+{
+    return(
+        <>
+            <NavBar />
+
+            <div className=''>
+
+
+            </div>
+            <Withdraw />
+        </>
+    );
+} 
