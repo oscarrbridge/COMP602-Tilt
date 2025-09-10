fddf96d131dc24a66e84211df043d2700518b065
Oscar Bridge <oscarbridge.nz@gmail.com>
2025-09-10 19:54:21 +1200

    Fixed styling from merge

--- src/components/NavWindow/NavWindow.tsx
+++ src/components/NavWindow/NavWindow.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom"
 
 export default function NavWindow() {
     const navigate = useNavigate();
-    
+     
     return(
         <>
             <div className='NavWindowContainer'>
