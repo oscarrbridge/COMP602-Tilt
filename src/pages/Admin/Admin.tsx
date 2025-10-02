6dc2d26342963af777204fc6086e548c80b923dc
Oscar Bridge <30382493+oscarrbridge@users.noreply.github.com>
2025-10-02 18:12:37 +1300

    Created staff and admin page. Staff page can create random code.

---
+++ src/pages/Admin/Admin.tsx
@@ -0,0 +1,15 @@
+import './Admin.css'
+import NavBar from '../../components/NavBar/NavBar.tsx'
+
+export default function Admin()
+{
+    return(
+        <>
+            <NavBar />
+            
+            <div>
+                hi
+            </div>
+        </>
+    );
+} 
